b1676249949446bf9f959b86b1b36182605d6125
msaadsohail05 <msaad.bahria@gmail.com>
2025-02-10 23:26:01 +0500

    Update and rename Question 01 to Question 01.cpp

---
+++ Lab 03/Question 01.cpp
@@ -0,0 +1,89 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+class MarkerPen{
+	private:
+		string brand;
+		string color;
+		int inklevel;
+		int refillability;
+	public:
+		MarkerPen(){
+			cout<<"Enter brand: ";
+			cin.ignore();
+			getline(cin,brand);
+			cout<<"Enter colour of the marker: ";
+			cin.ignore();
+			getline(cin,color);
+			cout<<"Enter ink level: ";
+			cin>>inklevel;
+			if(inklevel<10){
+				refillability = 1;
+			}
+			else if(inklevel ==10){
+				refillability = 0;
+			}
+		}
+		string GetBrand(){
+			return brand;
+		}
+		int GetInkLevel(){
+			return inklevel;
+		}
+		int GetRefillability(){
+			return refillability;
+		}
+		void SetInkLevel(int i){
+			inklevel = i;
+		}
+		void SetRefillability(int r){
+			refillability = r;
+		}
+		void write(){
+			if(inklevel>0){
+				cout<<"writing..."<<endl;
+				--inklevel;
+				refillability = 1;
+			}
+			else{
+				cout<<"Insufficient ink! Refill."<<endl;
+			}
+		}
+		void refill(){
+			if(refillability == 1){
+				inklevel = 10;
+				refillability = 0;
+				cout<<"Marker has been refilled!"<<endl;
+			}
+			else{
+				cout<<"Marker already full!"<<endl;
+			}
+		}
+};
+
+int main(){
+	int lvl;
+	MarkerPen p1;
+	int flag,choice;
+	flag = 1;
+	while(flag == 1){
+		cout<<"Enter 1 to write or 2 to refill the marker: ";
+		cin>>choice;
+		switch(choice){
+			case 1:
+				p1.write();
+				lvl = p1.GetInkLevel();
+				if(lvl == 1){
+					p1.refill();
+				}
+				break;
+			case 2:
+				p1.refill();
+				break;	
+		}
+		cout<<"Enter 1 to continue or any other number to stop: ";
+		cin>>flag;
+	}	
+	return 0;
+}
