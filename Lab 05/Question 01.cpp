1451b899ce136efd7bf0e742514c7fa5a21ea6be
msaadsohail05 <msaad.bahria@gmail.com>
2025-02-20 08:07:00 +0500

    Update Question 01.cpp

--- Lab 05/Question 01.cpp
+++ Lab 05/Question 01.cpp
@@ -1,3 +1,4 @@
+
 //Muahammad Saad Sohail  24K-0549
 
 #include<iostream>
@@ -6,33 +7,35 @@ using namespace std;
 
 class Car{
 	private:
-		string* RegistrationNumber;
-		string* ModelName;
-		string* OwnerName;
+		string RegistrationNumber;
+		string ModelName;
+		string OwnerName;
 	public:
 		Car(string reg,string mn,string on){
-			RegistrationNumber =new string(reg);
-			ModelName = new string(mn) ;
-			OwnerName = new string(on);
+			RegistrationNumber =reg;
+			ModelName = mn ;
+			OwnerName = on;
 		}
-		Car(Car& obj){
-			RegistrationNumber = new string(*obj.RegistrationNumber);	
-			ModelName = new string(*obj.ModelName);
-			OwnerName = obj.OwnerName;
+		Car(Car& obj, string own){
+			RegistrationNumber = obj.RegistrationNumber;	
+			ModelName = obj.ModelName;
+			OwnerName = own;
 		}
 		void Display(){
 			cout<<"Displaying Car Details:-"<<endl;
-			cout<<"Registration Number: "<<*RegistrationNumber<<endl;
-			cout<<"Model Name: "<<*ModelName<<endl;
-			cout<<"Owner Name: "<<*OwnerName<<endl;
+			cout<<"Registration Number: "<<RegistrationNumber<<endl;
+			cout<<"Model Name: "<<ModelName<<endl;
+			cout<<"Owner Name: "<<OwnerName<<endl;
 		}
 };
 
 int main(){
 	Car car1("sda213","x1","Mark");
-	Car car2(car1);
+	Car car2(car1,"saad");
+	cout<<"Car 1:-"<<endl;
 	car1.Display();
 	cout<<endl;
+	cout<<"Car 2:-"<<endl;
 	car2.Display();
 	
 	return 0;
