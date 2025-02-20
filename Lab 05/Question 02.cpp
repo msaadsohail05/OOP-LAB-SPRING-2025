f344eb659392c42cb0938f2783526bc254c56b24
msaadsohail05 <msaad.bahria@gmail.com>
2025-02-20 08:50:06 +0500

    Update Question 02.cpp

--- Lab 05/Question 02.cpp
+++ Lab 05/Question 02.cpp
@@ -1,1 +1,39 @@
 
+//Muahammad Saad Sohail  24K-0549
+
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+class Patient{
+	private:
+		int Id;
+		string Name;
+		int* tests;
+	public:
+		Patient(int id,string n,int t){
+			Id = id;
+			Name = n;
+			tests = new int(t);
+		}
+		Car(Car& obj){
+			RegistrationNumber = new string(*obj.RegistrationNumber);	
+			ModelName = new string(*obj.ModelName);
+			OwnerName = obj.OwnerName;
+		}
+		void Display(){
+			cout<<"Displaying Car Details:-"<<endl;
+			cout<<"Registration Number: "<<*RegistrationNumber<<endl;
+			cout<<"Model Name: "<<*ModelName<<endl;
+			cout<<"Owner Name: "<<*OwnerName<<endl;
+		}
+};
+
+int main(){
+	Car car1("sda213","x1","Mark");
+	Car car2(car1);
+	cout<<endl;
+	
+	return 0;
+}
+
