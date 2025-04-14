5bc5ea11f8e4cea12cb2ddc113c622a9b309bb07
msaadsohail05 <msaad.bahria@gmail.com>
2025-04-14 11:04:53 +0500

    Update Book.h

--- Lab 09/Book.h
+++ Lab 09/Book.h
@@ -1,1 +1,24 @@
+#ifndef Book
+#define Book
+
+class Book{
+  protected:
+    string title,author,ISBN;
+  public:
+    Book();
+    Book(string t,string a,string i);
+    void getTitle()=0;
+    void getAuthor()=0;
+    void getISBN()=0;
+};
+
+class Library: public Book{
+  public:
+    Book books[10];
+    void addBook() = 0;
+    void removeBook()=0;
+    void searchBook()=0;
+};
+
+#endif
 
