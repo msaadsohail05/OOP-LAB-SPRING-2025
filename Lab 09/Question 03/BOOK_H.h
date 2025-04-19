5f16d98bae13a6cd7145fa3c2065e5db32048910
msaadsohail05 <msaad.bahria@gmail.com>
2025-04-19 15:20:02 +0500

    Update BOOK_H.h

--- Lab 09/Question 03/BOOK_H.h
+++ Lab 09/Question 03/BOOK_H.h
@@ -1,66 +1,33 @@
-//Muhammad Saad Sohail    24K-0549
-
-#include "BOOK_H.hpp"
-#include "iostream"
-#include "string.h"
+#ifndef BookH
+#define BookH
+#include<string.h>
+#include<iostream>
 
 using namespace std;
-Book::Book(){
-  title =" ";
-  author = " ";
-  ISBN = " ";
-}
-Book::Book(string t,string a,string i): title(t), author(a), ISBN(i) {}
-string Book:: getTitle(){
-	return title;
-}
-string Book:: getAuthor(){
-  	return author;
-}
-string Book:: getISBN(){
-  	return ISBN;
-}
-Library::Library(){
-	count = 0;
-}
-void Library:: addBook(Book& b1){
-	if(count<20){
-    	books[count] = b1;
-    	count++;
-  	}
-  	else{
-    	cout<<"No space left to add another book!"<<endl;
-  	}
-}
 
-void Library:: removeBook(Book& b1){
-    int flag=0;
-    for(int i=0;i<count;i++){
-        if(books[count].getISBN() == b1.getISBN()){
-          count--;
-          cout<<"Book removed!"<<endl;
-          flag =1;
-        }  
-    }
-  if(flag == 1){
-    cout<<"Book "<<b1.getTitle()<<" has been removed!"<<endl;
-  }
-  else{
-    cout<<"Book "<<b1.getTitle()<<" was not found!"<<endl;
-  }
-}
+class Book{
+  protected:
+    string title,author,ISBN;
+  public:
+    Book();
+    Book(string t,string a,string i);
+    string getTitle();
+    string getAuthor();
+    string getISBN();
+    void setTitle(string s);
+    void setAuthor(string a);
+    void setISBN(string i);
+};
 
-void Library:: searchBook(Book &b1){
-	int flag=0;
-    for(int i=0;i<count;i++){
-        if(books[count].getISBN() == b1.getISBN()){
-          flag =1;
-        }  
-    }
-    if(flag == 1){
-    	cout<<"Book "<<b1.getTitle()<<" has been found!"<<endl;
-  	}
-  	else{
-    	cout<<"Book "<<b1.getTitle()<<" was not found!"<<endl;
-  	}
-}
+class Library: public Book{
+  public:
+    int count;
+    Library();
+    Book* books[20];
+    void addBook(Book& b1);
+    void removeBook(Book& b1);
+    void searchBook(Book& b1);
+    ~Library();
+};
+
+#endif
