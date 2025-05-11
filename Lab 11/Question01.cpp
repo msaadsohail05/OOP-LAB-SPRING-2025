b780aedd7b3a08a5eb818f6951e0ec7cd03be4ea
msaadsohail05 <msaad.bahria@gmail.com>
2025-05-11 23:35:30 +0500

    Update Question01.cpp

--- Lab 11/Question01.cpp
+++ Lab 11/Question01.cpp
@@ -1,1 +1,33 @@
 //Muhammad Saad Sohail        24K-0549
+
+Q1
+#include <iostream>
+#include <exception>
+
+using namespace std;
+
+struct InvalidValueException : exception {
+    const char* what() const noexcept override {
+        return "InvalidValueException - Age cannot be negative or exceed 120.";
+    }
+};
+
+void validate(int age){
+    if(age<0||age>120){
+      throw InvalidValueException();
+    } 
+}
+
+int main() {
+    int age;
+    cout << "Enter age: ";
+    cin >> age;
+    try{
+        validate(age);
+        cout << "Accepted age: " << age << endl;
+    } 
+    catch (const exception& ex){
+        cout << "Error: " << ex.what() << endl;
+    }
+    return 0;
+}
